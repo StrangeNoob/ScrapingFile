2532fa8ab33999dd2f53057c128c6015cc2b8864
StrangeNoob <sipun63427452@gmail.com>
2020-04-14 14:15:05 +0530

    Changes in clock.py

--- clock.py
+++ clock.py
@@ -2,10 +2,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from scraper import spreadSheet
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=10)
-def timed_job():
-    spreadSheet()
-
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=12)
 def scheduled_job():
     spreadSheet()
